b88e7a62b322ee09f8e0e9776cd9d576d50dde1f
Thiago Costa Santana <thikimlara@gmail.com>
2024-03-01 10:10:07 -0300

    Aula10 configurações

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import './App.css';
+import "./App.css";
 import { Router } from "./components";
 
 function App() {
+   // eslint-disable-next-line react/react-in-jsx-scope
    return <Router />;
 }
 
